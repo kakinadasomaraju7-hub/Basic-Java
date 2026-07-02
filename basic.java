fc578b6ced419788760f97719081c9a6c82e55df
Varalakshmi Kakinada <kakinadasomaraju7@gmail.com>
2026-07-02 21:33:44 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,18 @@
+ class Variables {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        double cgpa = 8.9;
+        char grade = 'A';
+        boolean placed = false;
+        String name = "Varalakshmi";
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("CGPA : " + cgpa);
+        System.out.println("Grade : " + grade);
+        System.out.println("Placed : " + placed);
+
+    }
+}
